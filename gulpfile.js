18653e40fd9b3b6294f6fa6fcf464f4c7a892090
deepfriedfilth <nkawatski@gmail.com>
2018-10-17 20:50:47 -0500

    Add hotfix for vulns + gulp v4 tweaks

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('styles', function() {
+gulp.task('styles', function(done) {
 	/* Sass it up, pack it up */
 	gulp.src('client/**/*.scss')
 	.pipe(sourcemaps.init())
@@ -10,8 +10,13 @@ gulp.task('styles', function() {
 	.pipe(sourcemaps.write('.'))
 	.pipe(gulp.dest('./client/'));
 
+	done();
 });
 
-gulp.task('default', ['styles'], function() {
-	gulp.watch('client/**/*.scss',['styles']);
+gulp.task('watch', function(done) {
+	gulp.watch('client/**/*.scss',gulp.parallel('styles'));
+
+	done();
 });
+
+gulp.task('default', gulp.series('styles', 'watch'));
